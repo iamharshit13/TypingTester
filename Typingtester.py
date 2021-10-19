75334a61104400b5e9eac198a3093bbc1f3dd133
Harshit Agrawal <42391556+iamharshit13@users.noreply.github.com>
2021-10-19 15:08:56 +0530

    Update Typingtester.py

--- Typingtester.py
+++ Typingtester.py
@@ -24,3 +24,36 @@ for linen in fullt:
         print()
         for letter in wordn:
             print(letter)
+
+"""
+This section is still under development
+will keep updating
+"""
+# taking values directly from keyboard
+def on_press(key):
+    try:
+        if key == 'q':
+            pass
+        else:
+            print("wrong")
+    except Error:
+        print(Error)
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press) as listener:
+    listener.join()
+    
+    
+    
+# For GUI Tkinter is used
+# Will develop this in the end
+from tkinter import *
+master = Tk()
+w = Canvas(master, width=40, height=60)
+w.pack()
+canvas_height=20
+canvas_width=200
+y = int(canvas_height / 2)
+w.create_line(0, y, canvas_width, y )
+mainloop()
